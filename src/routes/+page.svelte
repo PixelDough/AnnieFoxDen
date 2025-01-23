b5549ceddbb3e55bdc6ec9f94b3e17326fd1ecf7
PixelDough <alworrell19@gmail.com>
2025-01-22 20:09:43 -0600

    fixed padding on smaller screens, and replaced pfp with a higher quality version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-import AnnieTransparentSmall from '$lib/assets/AnnieTransparentSmall.png';
+import AnnieTransparentMedium from '$lib/assets/AnnieTransparentMedium.png';
 import '$lib/custom.css';
 // import "https://fonts.googleapis.com";
 // import "https://fonts.gstatic.com";
@@ -21,38 +21,40 @@ import '$lib/custom.css';
 <title>Annie's Fox Den</title>
 </svelte:head>
 
-<div class="header">
-	<img src={AnnieTransparentSmall} alt="Annie's Fox Den Icon"  />
-	Annie's Fox Den
+<div class="column">
+	<div class="header">
+		<img src={AnnieTransparentMedium} alt="Annie's Fox Den Icon"  />
+		Annie's Fox Den
+	</div>
+	<marquee>Coming soon...</marquee>
+	<p>
+		Welcome to my webpage! It's currently under construction. Here's a
+		list of what I plan on having here:
+	</p>
+	<ul>
+		<li>About Me</li>
+		<li>Portfolio/Resume</li>
+		<li>Ramblings</li>
+		<li>Project Showcases</li>
+	</ul>
+	
+	<p>In the meantime, here's a little bit about me</p>
+	<ul>
+		<li>Game Developer</li>
+		<li>Christian</li>
+		<li>Musician</li>
+		<li>3D Artist</li>
+		<li>Pronouns are she/her</li>
+		<li>Trans</li>
+		<li>Furry</li>
+	</ul>
+	
+	<p>And here's a couple links to teams I am a part of!</p>
+	<ul>
+		<li><a href="https://umeyu.me">UmeYume</a></li>
+		<li><a href="https://parallas.dev">parallas</a></li>
+	</ul>
+	
+	<hr \>
+	<p>Page built using Svelte</p>
 </div>
-<marquee>Coming soon...</marquee>
-<p>
-	Welcome to my webpage! It's currently under construction. Here's a
-	list of what I plan on having here:
-</p>
-<ul>
-	<li>About Me</li>
-	<li>Portfolio/Resume</li>
-	<li>Ramblings</li>
-	<li>Project Showcases</li>
-</ul>
-
-<p>In the meantime, here's a little bit about me</p>
-<ul>
-	<li>Game Developer</li>
-	<li>Christian</li>
-	<li>Musician</li>
-	<li>3D Artist</li>
-	<li>Pronouns are she/her</li>
-	<li>Trans</li>
-	<li>Furry</li>
-</ul>
-
-<p>And here's a couple links to teams I am a part of!</p>
-<ul>
-	<li><a href="https://umeyu.me">UmeYume</a></li>
-	<li><a href="https://parallas.dev">parallas</a></li>
-</ul>
-
-<hr \>
-<p>Page built using Svelte</p>
