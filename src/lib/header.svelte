6be6048faa1a1764aa7fb07c16001121bcfbcde0
PixelDough <alworrell19@gmail.com>
2025-01-24 18:53:53 -0600

    Added svelte fast marquee

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -2,6 +2,7 @@
 	let { pageName } = $props();
 	import '$lib/custom.css';
 	import AnnieTransparentMedium from '$lib/assets/AnnieTransparentMedium.png';
+	import Marquee from 'svelte-fast-marquee';
 </script>
 
 <div class="header">
@@ -10,4 +11,15 @@
 </div>
 <!-- <marquee>Coming soon...</marquee> -->
 
-<hr />
+
+<Marquee pauseOnHover={true} speed={50} play={true} gap="8px" style="mask-image: linear-gradient(to right, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 10%, rgba(255, 255, 255, 1) 90%, rgba(255, 255, 255, 0) 100%)">
+	<p> / </p>
+	<p> / </p>
+	<p> / </p>
+	<p>
+		This website is under active construction. Please be patient as the webmistress works!
+	</p>
+	<p> (>^3^)> </p>
+</Marquee>
+
+<!-- <hr /> -->
