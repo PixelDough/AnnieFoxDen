509d158a4d367f7bb1f783b7104484e55919c7d3
PixelDough <alworrell19@gmail.com>
2025-01-24 22:48:11 -0600

    Added more buttons

--- src/routes/friends/+page.svelte
+++ src/routes/friends/+page.svelte
@@ -4,8 +4,17 @@
 	
 	import AnnieButton from '$lib/assets/AnnieButton.gif';
 	
-	const imageModules = import.meta.glob(
-		'$lib/assets/buttons/*.{gif,jpg,png}',
+	const friendsButtons = import.meta.glob(
+		'$lib/assets/buttons/friends/*.{gif,jpg,png}',
+		{
+			eager: true,
+			query: {
+				enhanced: true
+			}
+		}
+	)
+	const coolSitesButtons = import.meta.glob(
+		'$lib/assets/buttons/cool/*.{gif,jpg,png}',
 		{
 			eager: true,
 			query: {
@@ -32,7 +41,20 @@
 	<p>Click on these buttons to go to their page!</p>
 	
 	<div style:display="flex" style:gap="8px" style:flex-wrap="wrap">
-		{#each Object.entries(imageModules) as [_path, module]}
+		{#each Object.entries(friendsButtons) as [_path, module]}
+			<a href={pathToUrl(_path)} aria-label={_path}><img src={module.default} alt="Go to site:{pathToUrl(_path)}" /></a>
+		{/each}
+	</div>
+</div>
+
+<hr />
+
+<div>
+	<h2>Cool sites' buttons</h2>
+	<p>Click on these buttons to go to their page!</p>
+	
+	<div style:display="flex" style:gap="8px" style:flex-wrap="wrap">
+		{#each Object.entries(coolSitesButtons) as [_path, module]}
 			<a href={pathToUrl(_path)} aria-label={_path}><img src={module.default} alt="Go to site:{pathToUrl(_path)}" /></a>
 		{/each}
 	</div>
