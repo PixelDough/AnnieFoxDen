7834b82e9251a38e1020c7990c3c79690d09a9be
PixelDough <alworrell19@gmail.com>
2025-07-22 01:03:41 -0500

    updated blog posts to use a more robust system to support alt text and links for images

--- src/lib/blogImages.svelte
+++ src/lib/blogImages.svelte
@@ -1,6 +1,7 @@
 <script>
 	const { images, height="200px" } = $props();
-	import HoverObject from "$lib/hoverObject.svelte"
+	import BlogImage from "$lib/blogImage.svelte";
+	import BlogItemCollection from "./blogItemCollection.svelte";
 </script>
 
 <style>
@@ -11,20 +12,8 @@
 	}
 </style>
 
-<div
-	style:display="flex"
-	style:flex-direction="row"
-	style:flex-wrap="wrap"
-	style:gap="8px"
-	style:justify-content="center"
->
-	{#each images as src }
-	<div style:max-width="49%">
-		<HoverObject>
-			<a href={src}>
-				<img src={src} alt="" style="height: {height}" />
-			</a>
-		</HoverObject>
-	</div>
+<BlogItemCollection>
+	{#each images as image }
+		<BlogImage src={image.src} alt={image.alt} link={image.link} height={height} />
 	{/each}
-</div>
+</BlogItemCollection>
---
+++ src/lib/blogItemCollection.svelte
@@ -0,0 +1,15 @@
+<script>
+	const { children } = $props();
+</script>
+
+<div
+	style:display="flex"
+	style:flex-direction="row"
+	style:flex-wrap="wrap"
+	style:gap="8px"
+	style:justify-content="center"
+>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
