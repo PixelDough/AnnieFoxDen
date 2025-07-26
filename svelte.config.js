5bb6a56440bd269e257c8e2e852d2684948ea93b
PixelDough <alworrell19@gmail.com>
2025-07-26 12:08:28 -0500

    migrated to new structure

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 // import adapter from 'svelte-adapter-nekoweb';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex'
+import { mdsvex } from 'mdsvex';
 import { createHighlighter } from 'shiki';
 
 // import { NEKOWEB_API_KEY, NEKOWEB_COOKIE } from '$env/static/private';
@@ -12,19 +12,18 @@ const highlighter = await createHighlighter({
 	langs: ['javascript', 'typescript']
 });
 
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex({extensions: [".svx", ".md"]})],
-	extensions: [".svelte", ".svx", ".md"],
+	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx', '.md'] })],
+	extensions: ['.svelte', '.svx', '.md'],
 
 	kit: {
 		adapter: adapter({
 			// apiKey: NEKOWEB_API_KEY,
 			// cookie: NEKOWEB_COOKIE,
-			folder: 'serve folder (default is "build")',
+			folder: 'build',
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
@@ -32,8 +31,8 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-	},
+		})
+	}
 };
 
 export default config;
